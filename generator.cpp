b451869cb87e9fe8898ad92ac51f19aa1afc43a3
Arattel <sizonov2000@gmail.com>
2019-05-20 01:16:18 +0300

    Added parallel generator(test), not safe

--- generator.cpp
+++ generator.cpp
@@ -6,7 +6,7 @@
 #include "mutator.h"
 
 #include <algorithm>
-
+#include "tbb/tbb.h"
 Generator::Generator(Selector &sel, Recombinator &rec, Mutator &m)
 	: selector(sel), recombinator(rec), mutator(m){}
 
@@ -30,3 +30,53 @@ void FullReplacementGenerator::new_generation(std::vector<individual> &populatio
 		population[i].second = fitness(population[i].first);
 	}
 }
+
+using namespace tbb;
+
+class ApplyFoo {
+	std::vector<individual> &population;
+	std::vector<individual> &old_population;
+	Selector &selector;
+	Recombinator &recombinator;
+	Mutator &mutator;
+	fitness_function fitnessFunction;
+public:
+	ApplyFoo(std::vector<individual> &population,
+			 Selector &selector,
+			 Recombinator &recombinator,
+			 Mutator &mutator, std::vector<individual> &old_population, fitness_function &fit) : population(population),
+																								 selector(selector),
+																								 recombinator(
+																										 recombinator),
+																								 mutator(mutator),
+																								 old_population(
+																										 old_population),
+																								 fitnessFunction(fit) {
+		population = population;
+		selector = selector;
+		recombinator = recombinator;
+		mutator = mutator;
+		old_population = old_population;
+		fitnessFunction = fit;
+	}
+
+	void operator()(const blocked_range<size_t> &r) const {
+		for (size_t i = r.begin(); i != r.end(); ++i) {
+			std::pair<size_t, size_t> parents = selector.get_pair();
+			population[i].first = mutator.mutate(recombinator.crossover(
+					old_population[parents.first].first, old_population[parents.second].first));
+			population[i].second = fitnessFunction(population[i].first);
+		}
+	}
+
+};
+
+void FullReplacementGenerator::new_generation_parallel(std::vector<individual> &population, fitness_function fitness) {
+	std::vector<individual> old_population(population);
+	selector.set_population(old_population);
+	parallel_for(blocked_range<size_t>(0, population.size()),
+				 ApplyFoo(population, selector, recombinator, mutator, old_population, fitness));
+
+}
+
+
--- generator.h
+++ generator.h
@@ -11,14 +11,16 @@ class Mutator;
 class Generator
 {
 protected:
+
+public:
 	Selector& selector;
 	Recombinator& recombinator;
 	Mutator& mutator;
-public:
 	Generator(Selector& sel, Recombinator& rec, Mutator& m);
 	// TODO make population & fitness passed to the class only once
 	virtual void create_population(std::vector<individual>& population, fitness_function fitness);
 	virtual void new_generation(std::vector<individual> &population, fitness_function fitness) = 0;
+	virtual void new_generation_parallel(std::vector<individual> &population, fitness_function fitness) = 0;
 };
 
 class FullReplacementGenerator: public Generator
@@ -27,6 +29,7 @@ public:
 	FullReplacementGenerator(Selector& sel, Recombinator& rec, Mutator& m):
 		Generator(sel, rec, m) {}
 	virtual void new_generation(std::vector<individual> &population, fitness_function fitness);
+	virtual void new_generation_parallel(std::vector<individual> &population, fitness_function fitness);
 };
 
 #endif // GENERATOR_H
